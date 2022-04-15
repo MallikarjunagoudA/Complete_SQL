9b3acd0fea3ed69f44078d9d8b4d12c29e2a36f9
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-04-15 14:49:30 +0530

    joins

---
+++ SQL Queries/Joins.sql
@@ -0,0 +1,83 @@
+--JOINS
+
+
+--creating the tables
+create table teachers ( dprtName varchar(50) primary key,teacherID int ,teacherName varchar(50))
+create table TeachersDetails (ID int primary key identity(1,1), teacherName varchar(50), phone varchar(50), email varchar(50), address varchar(50), dob date)
+
+
+--adding the foreign key
+alter table teachersDetails
+add constraint teacherid_fk foreign key(teacherid) references teachers(teacherid)
+
+
+--drop the fk
+alter table teachersDetails
+drop constraint teacherid_fk
+
+
+--drop tables
+drop table teachers
+drop table TeachersDetails
+
+--drop column
+alter table teachers
+drop column teacherName
+
+--adding the foreign key
+alter table teachers
+add constraint teacherid_fk foreign key(teacherid) references teachersDetails(id)
+
+--adding the values
+
+insert into  TeachersDetails values('karne V','0000000000000', 'v@gmail.com', 'gGada','09/07/1787')
+insert into  TeachersDetails values('rm bingi','0000000000000', 'v@gmail.com', 'gGada','09/07/1787')
+insert into  TeachersDetails values('nisar T','0000000000000', 'v@gmail.com', 'gGada','09/07/1787')
+
+insert into  TeachersDetails values('hosalli V','0000000000000', 'v@gmail.com', 'gGada','09/07/1787')
+insert into  TeachersDetails values('rm karthik','0000000000000', 'v@gmail.com', 'gGada','09/07/1787')
+insert into  TeachersDetails values('herakal T','0000000000000', 'v@gmail.com', 'gGada','09/07/1787')
+
+
+
+insert into teachers values('science',2)
+insert into teachers values('mathes',2)
+insert into teachers values('english',3)
+insert into teachers values('hindi',3)
+insert into teachers values('kannada',1)
+insert into teachers values('social',1)
+
+
+--validate
+select * from teachers
+select * from teachersdetails
+
+--
+---------------------joins------------------------------
+
+--1. join 
+
+select * from teachers a join teachersdetails b on a.teacherid=b.id
+
+--inner join
+
+select * from teachers a inner join teachersdetails b on a.teacherid=b.id
+
+--lfet join or left outer join
+select * from teachers a left join teachersdetails b on a.teacherid=b.id
+
+select * from teachers a left outer join teachersdetails b on a.teacherid=b.id
+
+--right join or right outer join
+
+select * from teachers a right join teachersdetails b on a.teacherid=b.id
+
+select * from teachers a right outer join teachersdetails b on a.teacherid=b.id
+ 
+
+ --outer join or full outer join
+ select * from teachers a full join teachersdetails b on a.teacherid=b.id
+
+select * from teachers a full outer join teachersdetails b on a.teacherid=b.id
+
+--
---
+++ SQL Queries/grp by and agret function.sql
@@ -0,0 +1,41 @@
+--group by
+
+--when ever we are using the group by clause the we need to select the column carefully. the columns which are used in the select 
+-----clause that are need to either in group by clause or used with aggregate functions.
+
+
+
+
+USE [office1dbPractice]
+GO
+
+SELECT 
+      [frdName]
+      ,[phoneNo]
+      ,[email]
+  FROM [dbo].[friends]
+
+GO
+
+insert into friends values('manu1','00000000','m@gamil.com')
+insert into friends values('manu2','00000000','m1@gamil.com')
+insert into friends values('manu3','00000000','m2@gamil.com')
+insert into friends values('manu1','000000001','m3@gamil.com')
+
+insert into friends values('manu1','000000002','m4@gamil.com')
+
+
+--using group by
+
+select frdName,Count(frdName) as sameNameOfFriends from friends group by frdName 
+
+--aggregate functions:
+--count,sum,min,max,avg
+
+
+--using Having used for filter
+
+select frdName,Count(frdName) as sameNameOfFriends from friends group by frdName having frdName like '%1%'	
+
+
+
