b5836ec5068593662705cd80bec3722202719e57
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-04-16 16:13:16 +0530

    store procedure

--- SQL Queries/store procedure.sql
+++ SQL Queries/store procedure.sql
@@ -2,7 +2,14 @@
 
 --create table
 create table emp (name varchar(max), age int)
+select * from emp
 
+--sql injection into out of the sp
+declare @name varchar(max) 
+set @name='--manoj'
+select *  from emp where name=@name and age=29    
+
+select * from emp where age=29 and name='jan'  
 
 ---create store procedure
 create procedure use_addDeails
@@ -12,7 +19,7 @@ as
 begin
 insert into emp values(@name, @age)
 
-select * from emp
+
 end
 
 
@@ -24,7 +31,7 @@ use_addDeails 'sujit',24
 exec use_addDeails 'sujit',24
 
 --3
-execute use_addDeails 'sujit',24
+execute use_addDeails '--manoj',34
 
 
 
@@ -41,7 +48,7 @@ insert into emp values(@name, @age)
 end
 --insert into emp values(@name, @age)
 
-select * from emp
+select * from emp where name=@name and age=29
 end
 
 
@@ -49,3 +56,21 @@ end
 --drop procedure
 
 drop procedure use_addDeails
+
+
+
+--sql injection
+
+
+declare @name varchar(max) 
+set @name='gana'
+print(@name)
+exec ('select * from emp where name='+@name)
+
+
+--sp advantages:
+--security
+--resuablity
+--exe plan redundanancy
+--can be encode
+--sql injection will be not able to do for sp
